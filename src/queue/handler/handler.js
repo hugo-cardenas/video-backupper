ca9f73abd835fdcba6d90115a518aa2534f38dad
hugo <hcgar00@gmail.com>
2016-11-08 21:48:34 +0200

    Implement job handler

--- src/queue/handler/handler.js
+++ src/queue/handler/handler.js
@@ -1,17 +1,72 @@
 var url = require('url');
+var _ = require('lodash');
 var VError = require('verror');
 
 module.exports = function (ytdl, storage, displayOutput) {
+    /**
+     * Handle a queue job
+     * @param {Object} job BeeQueue job object
+     * @returns {Promise}
+     */
     function handle(job) {
-        job.data
+        return Promise.resolve()
+            .then(function () {
+                var jobData = getValidatedJobData(job);
+                return backup(jobData.playlistId, jobData.videoId);
+            })
+            .catch(function (err) {
+                return Promise.reject(createError(job, err));
+            });
     }
 
-    function getVideoItem() {
-        
+    /**
+     * Backup one specific video (download and save)
+     *
+     * @param {string} playlistId Playlist to which the video belongs
+     * @param {string} videoId
+     * @returns {Promise}
+     */
+    function backup(playlistId, videoId) {
+        return new Promise(function (resolve, reject) {
+            try {
+                var url = buildVideoUrl(videoId);
+                var stream = ytdl(url);
+            } catch (err) {
+                return reject(createError(videoId, err));
+            }
+
+            return storage.save(stream, playlistId, videoId)
+                .then(function () {
+                    displayOutput.outputLine('Saved video ' + videoId);
+                    return resolve();
+                })
+                .catch(function (err) {
+                    return reject(createError(videoId, err));
+                });
+        });
     }
 
-    function validateJobData(job) {
+    /**
+     * @param {Object} job
+     * @returns {Object}
+     */
+    function getValidatedJobData(job) {
+        validateProperties(job, ['data']);
+        validateProperties(job.data, ['videoId', 'playlistId']);
+        return job.data;
+    }
 
+    /**
+     * Validate that object contains mandatory properties
+     * @param {Object} object
+     * @param {string[]} mandatoryProperties
+     * @throws {Error} If object is missing any mandatory property
+     */
+    function validateProperties(object, mandatoryProperties) {
+        var missingProperties = _.difference(mandatoryProperties, Object.keys(object));
+        if (missingProperties.length > 0) {
+            throw new Error('Missing properties [' + missingProperties.join(', ') + '] in object ' + JSON.stringify(object));
+        }
     }
 
     /**
@@ -28,12 +83,12 @@ module.exports = function (ytdl, storage, displayOutput) {
     }
 
     /**
-     * @param {Object} jobData
+     * @param {Object} job
      * @param {Error} err
      * @returns {Error}
      */
-    function createError(jobData, err) {
-        var error = new VError(err, 'Unable to handle job %s', JSON.stringify(jobData));
+    function createError(job, err) {
+        var error = new VError(err, 'Unable to handle job %s', JSON.stringify(job));
         displayOutput.outputLine(error.message);
         return error;
     }
--- test/unit/queue/handler/handler.test.js
+++ test/unit/queue/handler/handler.test.js
@@ -25,8 +25,10 @@ test('handler - handle - succeeds', function (t) {
     };
 
     var job = {
-        videoId: videoId,
-        playlistId: playlistId
+        data: {
+            videoId: videoId,
+            playlistId: playlistId
+        }
     };
 
     var handler = createHandler(ytdl, storage, displayOutput);
@@ -37,14 +39,107 @@ test('handler - handle - succeeds', function (t) {
         });
 });
 
-test('handler - handle - invalid job', function (t) {
+var invalidJobs = [
+    {},
+    { foo: 123 },
+    { data: {} },
+    { data: { playlistId: 42 } },
+    { data: { videoId: 44 } }
+];
 
+invalidJobs.forEach(function (job, index) {
+    test('handler - handle - invalid job #' + index, function (t) {
+        var ytdl = sinon.stub();
+
+        var storage = { save: sinon.stub() };
+        var displayOutput = { outputLine: sinon.stub() };
+
+        var handler = createHandler(ytdl, storage, displayOutput);
+        return handler.handle(job)
+            .then(function () {
+                t.fail();
+            })
+            .catch(function (err) {
+                t.ok(err.message.includes(JSON.stringify(job)));
+                t.ok(displayOutput.outputLine.calledWith(err.message));
+            });
+    });
 });
 
 test('handler - handle - ytdl fails', function (t) {
+    var playlistId = 'playlist42';
+    var videoId = 'foo42';
 
+    var baseVideoUrl = 'https://www.youtube.com/watch?v=';
+    var expectedVideoUrl = baseVideoUrl + videoId;
+
+    var errorMessage = 'Ytdl failed';
+    var ytdl = sinon.stub()
+        .withArgs(expectedVideoUrl)
+        .throws(new Error(errorMessage));
+
+    var storage = { save: sinon.stub() };
+
+    var displayOutput = {
+        outputLine: sinon.stub()
+    };
+
+    var job = {
+        data: {
+            videoId: videoId,
+            playlistId: playlistId
+        }
+    };
+
+    var handler = createHandler(ytdl, storage, displayOutput);
+    return handler.handle(job)
+        .then(function () {
+            t.fail();
+        })
+        .catch(function (err) {
+            t.ok(err.message.includes(JSON.stringify(job)));
+            t.ok(err.message.includes(errorMessage));
+            t.ok(displayOutput.outputLine.calledWith(err.message));
+        });
 });
 
 test('handler - handle - storage fails', function (t) {
+    var playlistId = 'playlist42';
+    var videoId = 'foo42';
+    var stream = 'myStream42';
 
+    var baseVideoUrl = 'https://www.youtube.com/watch?v=';
+    var expectedVideoUrl = baseVideoUrl + videoId;
+
+    var ytdl = sinon.stub()
+        .withArgs(expectedVideoUrl)
+        .returns(stream);
+
+    var errorMessage = 'Storage failed';
+    var storage = { save: sinon.stub() };
+    storage.save
+        .withArgs(stream, playlistId, videoId)
+        .returns(Promise.reject(new Error(errorMessage)));
+
+    var displayOutput = {
+        outputLine: sinon.stub()
+    };
+
+    var job = {
+        data: {
+            videoId: videoId,
+            playlistId: playlistId
+        }
+    };
+
+    var handler = createHandler(ytdl, storage, displayOutput);
+    return handler.handle(job)
+        .then(function () {
+            t.fail();
+        })
+        .catch(function (err) {
+            t.ok(err.message.includes(JSON.stringify(job)));
+            t.ok(err.message.includes(errorMessage));
+            t.ok(displayOutput.outputLine.calledWith(err.message));
+        });
 });
